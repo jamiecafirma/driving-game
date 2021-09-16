aad7a234058fd7bf7bb1727470bb82e1a9a9a174
Jamie Cafirma <jamie.cafirma@gmail.com>
2021-09-16 15:45:33 -0700

    Add a location property to the model to keep track of the car's coordinates on the page. Set the car's position to absolute in relation to the background div. Listen for keydown events on the space bar and move the car a few pixels every 16 milliseconds, then update the car's position in the data model. #5

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 var data = {
-  carDirection: 'east'
+  direction: 'east',
+  location: {
+    x: 0,
+    y: 0
+  },
+  currentInterval: 0
 };
 
 var $car = document.querySelector('.car');
@@ -7,17 +12,30 @@ var $car = document.querySelector('.car');
 function turnCar(event) {
   if (event.key === 'ArrowDown') {
     $car.className = 'car south';
-    data.carDirection = 'south';
+    data.direction = 'south';
   } else if (event.key === 'ArrowUp') {
     $car.className = 'car north';
-    data.carDirection = 'north';
+    data.direction = 'north';
   } else if (event.key === 'ArrowRight') {
     $car.className = 'car east';
-    data.carDirection = 'east';
+    data.direction = 'east';
   } else if (event.key === 'ArrowLeft') {
     $car.className = 'car west';
-    data.carDirection = 'west';
+    data.direction = 'west';
+  }
+}
+
+function moveCarForward() {
+  data.location.x += 8;
+  $car.style.left = data.location.x + 'px';
+}
+
+function startCar(event) {
+  if (event.key === ' ') {
+    data.currentInterval = setInterval(moveCarForward, 16);
   }
 }
 
 document.addEventListener('keydown', turnCar);
+
+document.addEventListener('keydown', startCar);
